2c94fa92bc2c7905d9391f1f890060fded45e4a8
Jeff Spijkerboer <jeff.spijkerboer@avanade.com>
2023-10-10 11:41:25 +0200

    .Net DateOnly extension, convert to DateTimeOffset

---
+++ .NET/DateOnlyExtension.cs
@@ -0,0 +1,18 @@
+﻿public static class DateOnlyExtension
+{
+    public static DateTimeOffset ToDateTimeOffset(this DateOnly dateOnly, TimeZoneInfo timeZoneInfo)
+    {
+        var dateTime = dateOnly.ToDateTime(TimeOnly.MinValue);
+        return new DateTimeOffset(dateTime, timeZoneInfo.GetUtcOffset(dateTime));
+    }
+
+    public static DateTimeOffset? ToDateTimeOffset(this DateOnly? dateOnly, TimeZoneInfo timeZoneInfo)
+    {
+        if (dateOnly.HasValue)
+        {
+            var dateTime = dateOnly.Value.ToDateTime(TimeOnly.MinValue);
+            return new DateTimeOffset(dateTime, timeZoneInfo.GetUtcOffset(dateTime));
+        }
+        return null;
+    }
+}
